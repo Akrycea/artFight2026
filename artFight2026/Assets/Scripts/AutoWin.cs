8df15b2ae9bd0da1af9c4b329823a47d32acbe22
Akrycea <smagibag@gmail.com>
2026-07-27 16:23:23 +0200

    Small changes i dont have time to fix fully :(

--- artFight2026/Assets/Scripts/AutoWin.cs
+++ artFight2026/Assets/Scripts/AutoWin.cs
@@ -3,23 +3,29 @@ using UnityEngine.UI;
 
 public class AutoWin : MonoBehaviour
 {
-
     public void autoWin()
     {
+        Win();
+    }
+    public GameObject Win()
+    {
+        Debug.Log("doing auto win");
         // Gather all GameObjects (including inactive ones) in the scene
         GameObject[] allObjects = GameObject.FindGameObjectsWithTag("Prize");
         foreach (GameObject obj in allObjects)
         {
+            Debug.Log("found obejcts:" + obj.name);
             // Ensure the object has a SpriteRenderer component
             Image spriteRenderer = obj.GetComponent<Image>();
 
             // Check if the SpriteRenderer exists and if the sprite name matches
             if (spriteRenderer != null && spriteRenderer.sprite != null)
             {
-                    spriteRenderer.color = new Color32(255, 255, 255, 255);
+                Debug.Log("changing color");
+                spriteRenderer.color = new Color32(255, 255, 255, 255);
             }
         }
 
-        //return null; // Not found
+        return null; // Not found
     }
 }
--- artFight2026/Assets/Scripts/Bug.cs
+++ artFight2026/Assets/Scripts/Bug.cs
@@ -7,15 +7,6 @@ using UnityEngine;
 public class Bug : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
-
-    //[SerializeField] private float minDistance = 1f;
-    //[SerializeField] private float maxDistance = 5f;
-    //[SerializeField] private Vector2 targetPosition;
-    //[SerializeField] private float t = 0;
-
-    //[SerializeField] private float timeBetweenChangingDirection = 1f;
-    //private bool canMove;
-
     [SerializeField] private float timeToDisappear;
 
     [SerializeField] List<Sprite> sprites;
@@ -26,7 +17,6 @@ public class Bug : MonoBehaviour
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         timeToDisappear = Random.Range(3f, 8f);
-        //canMove = true;
         chooseASprite();
     }
 
@@ -38,28 +28,8 @@ public class Bug : MonoBehaviour
 
     void Update()
     {
-        //if (canMove)
-        //{
-        //    // save it current rotation
-        //    Quaternion rotation = transform.rotation;
-        //    // Spin it around a random amount
-        //    transform.Rotate(Vector3.forward, Random.Range(0, 360));
-        //    // move it a random amount forward based on it current facing
-        //    transform.position += transform.up * Random.Range(minDistance, maxDistance);
-        //    // put it back to its original rotation
-        //    transform.rotation = rotation;
-        //    StartCoroutine(wait());
-        //}
-
         bugDies();
     }
-
-    //private IEnumerator wait()
-    //{
-    //    canMove = false;
-    //    yield return new WaitForSeconds(timeBetweenChangingDirection);
-    //    canMove = true;
-    //}
 
     private void OnMouseDown()
     {
